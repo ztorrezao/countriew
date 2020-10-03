a39c8eabd079a849c69a2681af7d533218642ded
Zacarias Jorge Torrezão <z.torrezao@gmail.com>
2020-10-03 18:59:50 +0200

    :link: Add base html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import {
+  List,
+  Search,
+  Download,
+  ChevronDown,
+  Grid,
+  Table,
+  Flag,
+  Grid3x2
+} from 'ngx-bootstrap-icons';
+
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { CountriesComponent } from './countries/countries.component';
+import { CountryComponent } from './country/country.component';
+import { CountryCardComponent } from './country-card/country-card.component';
+import { AboutComponent } from './about/about.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+const icons = {
+  List,
+  Search,
+  Download,
+  ChevronDown,
+  Grid,
+  Table,
+  Flag,
+  Grid3x2
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    CountriesComponent,
+    CountryComponent,
+    CountryCardComponent,
+    AboutComponent,
+    SearchBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxBootstrapIconsModule.pick(icons)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/country-card/country-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-card',
+  templateUrl: './country-card.component.html',
+  styleUrls: ['./country-card.component.scss']
+})
+export class CountryCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
