beb714596c1206d4757d1f2fb250b1aba09b6c75
Zacarias Jorge Torrezão <z.torrezao@gmail.com>
2020-10-06 07:54:12 +0200

    :sparkles: Add square kilometer pipe

--- src/app/square-kilometer.pipe.ts
+++ src/app/square-kilometer.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SquareKilometerPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number): string {
+    return `${value} km²`;
   }
 
 }
