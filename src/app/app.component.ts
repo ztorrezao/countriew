6f4ceafb16b8686c7181952e257683f4bc47690c
Zacarias Jorge Torrezão <z.torrezao@gmail.com>
2020-10-06 04:26:36 +0200

    :sparkles: Add open and close methods to the mobile nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Countriew';
+  title: string = 'Countriew';
+  isMobileNavOpen: boolean = false;
+
+  openMobileNav() {
+    this.isMobileNavOpen = true;
+  }
+
+  closeMobileNav() {
+    this.isMobileNavOpen = false;
+  }
 }
